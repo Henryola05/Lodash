449460f92594cc0f3304bf8605c57d2c8103735a
Jegede Olatide <60626256+Henryola05@users.noreply.github.com>
2020-06-11 15:41:15 -0500

    Uploading the chunk method

--- main.js
+++ main.js
@@ -71,6 +71,14 @@ const _ = {
     let dropNumber = array.findIndex(cb);
     let droppedArray = this.drop(array, dropNumber);
     return droppedArray;
+  },
+  chunk(array, size=1){
+    let arrayChunks = [];
+    for(let i = 0; i < array.length; i += size){
+      let arrayChunk = array.slice(i, i+size);
+      arrayChunks.push(arrayChunk);
+    }
+    return arrayChunks
   }
 
 };
